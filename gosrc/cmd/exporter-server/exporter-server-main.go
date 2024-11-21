d16cc0180459f52886bd8200167ce169fca2ea65
Ben Wirth <benjaminwirth10@gmail.com>
2024-11-20 22:13:51 -0800

    Go entrypoints -
    Main files for both the config setup and server running.

---
+++ gosrc/cmd/exporter-server/exporter-server-main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/benw10-1/brotato-exporter/errutil"
+	"github.com/benw10-1/brotato-exporter/exporterserver"
+	"github.com/benw10-1/brotato-exporter/exporterserver/ctrlauth"
+	"github.com/benw10-1/brotato-exporter/exporterserver/ctrlmessage"
+	"github.com/benw10-1/brotato-exporter/exporterserver/messagesubhandler"
+	"github.com/benw10-1/brotato-exporter/exporterstore"
+	"github.com/spf13/viper"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+func loadYAMLConfig() error {
+	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
+	viper.AutomaticEnv()
+
+	viper.SetConfigName("default")
+
+	viper.AddConfigPath("/etc/brotatoexporter")
+	viper.AddConfigPath("/var/brotatoexporter")
+	viper.SetConfigType("yaml")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return errutil.NewStackError(err)
+	}
+
+	viper.SetConfigName("override")
+
+	err = viper.MergeInConfig()
+	if err != nil {
+		_, isNotFound := err.(viper.ConfigFileNotFoundError)
+		if !isNotFound {
+			return errutil.NewStackError(err)
+		}
+
+		// generate base config override file - this is just the auth key for now so not importing the yaml writer stuff
+		overrideFile, err := os.Create("/var/brotatoexporter/override.yaml")
+		if err != nil {
+			return errutil.NewStackError(err)
+		}
+		defer overrideFile.Close()
+
+		authKeyRaw := make([]byte, 32)
+
+		_, err = rand.Read(authKeyRaw)
+		if err != nil {
+			return errutil.NewStackError(err)
+		}
+
+		authKey := base64.StdEncoding.EncodeToString(authKeyRaw)
+
+		_, err = overrideFile.WriteString(fmt.Sprintf(`jwt-auth-signing-key: "%s"`, authKey))
+		if err != nil {
+			return errutil.NewStackError(err)
+		}
+
+		viper.Set("jwt-auth-signing-key", authKey)
+
+		log.Println("No override config file found, created one at /var/brotatoexporter/override.yaml")
+	}
+
+	return nil
+}
+
+func main() {
+	appCtx, cancelAppCtx := context.WithCancel(context.Background())
+	defer cancelAppCtx()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-sigChan
+		cancelAppCtx()
+	}()
+
+	appLogWriter := &lumberjack.Logger{
+		Filename:   "/var/log/exporter-server-app.log",
+		MaxSize:    50,
+		MaxBackups: 3,
+		Compress:   true,
+	}
+	log.SetOutput(appLogWriter)
+
+	requestLogWriter := &lumberjack.Logger{
+		Filename:   "/var/log/exporter-server-requests.log",
+		MaxSize:    50,
+		MaxBackups: 3,
+		Compress:   true,
+	}
+	requestLogger := log.New(requestLogWriter, "", 0)
+
+	go func() {
+		http.ListenAndServe(viper.GetString("pprof-serve-addr"), nil)
+	}()
+
+	err := loadYAMLConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	exporterStore, err := exporterstore.NewExporterStore("/var/brotatoexporter/user.db")
+	if err != nil {
+		panic(err)
+	}
+	defer exporterStore.Close()
+
+	sessionInfoMap := new(ctrlauth.SessionInfoMap)
+
+	handlerList := make([]http.Handler, 0)
+
+	authAPI := ctrlauth.NewAuthAPI([]byte(viper.GetString("jwt-auth-signing-key")), sessionInfoMap, exporterStore)
+	handlerList = append(handlerList, authAPI)
+
+	subHandler := messagesubhandler.NewMessageSubHandler()
+
+	messageAPI := ctrlmessage.NewMessageAPI(sessionInfoMap, exporterStore, subHandler)
+	handlerList = append(handlerList, messageAPI)
+
+	exporterServer := exporterserver.NewExporterServer(handlerList, requestLogger)
+
+	srv := http.Server{
+		Addr:         viper.GetString("serve-addr"),
+		Handler:      exporterServer,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 10,
+	}
+
+	go func() {
+		<-appCtx.Done()
+		shutdownCtx, cancelShutdownCtx := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancelShutdownCtx()
+
+		err = srv.Shutdown(shutdownCtx)
+		log.Printf("Server shutdown with err: %v", err)
+	}()
+
+	log.Printf("Server listening on %s", srv.Addr)
+
+	err = srv.ListenAndServe()
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
+		log.Printf("Server error: %v", err)
+	}
+}
---
+++ gosrc/cmd/mod-user-create/mod-user-create.go
@@ -0,0 +1,206 @@
+package main
+
+import (
+	"archive/zip"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/benw10-1/brotato-exporter/brotatomod/brotatomodtypes"
+	"github.com/benw10-1/brotato-exporter/errutil"
+	"github.com/benw10-1/brotato-exporter/exporterstore"
+	"github.com/benw10-1/brotato-exporter/exporterstore/exporterstoretypes"
+	"github.com/google/uuid"
+)
+
+var (
+	host           string
+	port           int
+	https          bool
+	verifyHost     bool
+	maxSubscribers int
+)
+
+func prompts() {
+	var prompt survey.Prompt
+	prompt = &survey.Input{
+		Message: "Host",
+		Default: "127.0.0.1",
+	}
+	err := survey.AskOne(prompt, &host)
+	if err != nil {
+		panic(err)
+	}
+
+	prompt = &survey.Input{
+		Message: "Port",
+		Default: "8080",
+	}
+	err = survey.AskOne(prompt, &port)
+	if err != nil {
+		panic(err)
+	}
+
+	prompt = &survey.Confirm{
+		Message: "HTTPS",
+		Default: false,
+	}
+	err = survey.AskOne(prompt, &https)
+	if err != nil {
+		panic(err)
+	}
+
+	prompt = &survey.Confirm{
+		Message: "Verify Host",
+		Default: false,
+	}
+	err = survey.AskOne(prompt, &verifyHost)
+	if err != nil {
+		panic(err)
+	}
+
+	prompt = &survey.Input{
+		Message: "Max Subscribers",
+		Default: "5",
+	}
+	err = survey.AskOne(prompt, &maxSubscribers)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	prompts()
+	exporterStore, err := exporterstore.NewExporterStore("/var/brotatoexporter/user.db")
+	if err != nil {
+		panic(err)
+	}
+	defer exporterStore.Close()
+
+	authKeyRaw := make([]byte, 32)
+
+	_, err = rand.Read(authKeyRaw)
+	if err != nil {
+		panic(err)
+	}
+
+	authKey := base64.StdEncoding.EncodeToString(authKeyRaw)
+
+	user := &exporterstoretypes.ExporterUser{
+		UserID:         uuid.New(),
+		MaxSubscribers: maxSubscribers,
+	}
+
+	err = exporterStore.UpsertUser(user)
+	if err != nil {
+		panic(err)
+	}
+
+	err = exporterStore.UpsertAuthKeyUserID([]byte(authKey), user.UserID)
+	if err != nil {
+		panic(err)
+	}
+
+	config := brotatomodtypes.ModConfig{
+		Enabled: true,
+		ConnectionData: brotatomodtypes.ModConfigConnectionData{
+			Host:       host,
+			Port:       port,
+			HTTPS:      https,
+			VerifyHost: verifyHost,
+			AuthToken:  authKey,
+		},
+	}
+
+	makeConfigFile(config)
+	makeUserModZip()
+
+	log.Printf("User created with ID (%s) with config - %+v", user.UserID, config)
+}
+
+func makeUserModZip() {
+	zipFile, err := os.Create("/var/brotatoexporter/user-mod.zip")
+	if err != nil {
+		panic(err)
+	}
+	defer zipFile.Close()
+
+	zipWriter := zip.NewWriter(zipFile)
+	defer zipWriter.Close()
+
+	baseDir := "/var/lib/mod"
+
+	err = filepath.Walk(baseDir, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return errutil.NewStackError(err)
+		}
+		path = path[len(baseDir):]
+
+		if path == "" {
+			return nil
+		}
+		// remove leading slash
+		path = path[1:]
+		fmt.Println("Zipping -", path)
+
+		if info.IsDir() {
+			_, err = zipWriter.Create(fmt.Sprintf("%s%c", path, os.PathSeparator))
+
+			return errutil.NewStackError(err)
+		}
+
+		file, err := os.Open(filepath.Join(baseDir, path))
+		if err != nil {
+			return errutil.NewStackError(err)
+		}
+		defer file.Close()
+
+		f, err := zipWriter.Create(path)
+		if err != nil {
+			return errutil.NewStackError(err)
+		}
+
+		_, err = io.Copy(f, file)
+		if err != nil {
+			return errutil.NewStackError(err)
+		}
+
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func makeConfigFile(config brotatomodtypes.ModConfig) {
+	err := os.MkdirAll("/var/brotatoexporter", 0755)
+	if err != nil {
+		panic(err)
+	}
+
+	outputConfigFile, err := os.Create("/var/brotatoexporter/connect-config.json")
+	if err != nil {
+		panic(err)
+	}
+	defer outputConfigFile.Close()
+
+	zipTargetConfigFile, err := os.Create("/var/lib/mod/mods-unpacked/benw10-BrotatoExporter/connect-config.json")
+	if err != nil {
+		panic(err)
+	}
+	defer zipTargetConfigFile.Close()
+
+	multiWriter := io.MultiWriter(outputConfigFile, zipTargetConfigFile)
+
+	err = json.NewEncoder(multiWriter).Encode(config)
+	if err != nil {
+		panic(err)
+	}
+}
